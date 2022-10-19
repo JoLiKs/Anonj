e8940c856ac24181cd92603d1750b9a1f8bf5ffe
Egor Ralovich <vonkad002@gmail.com>
2022-10-19 22:24:13 +0300

    Add files via upload

---
+++ src/main/java/com/WebConfig.java
@@ -0,0 +1,19 @@
+package com.example.messagingstompwebsocket;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*");
+    }
+
+}
